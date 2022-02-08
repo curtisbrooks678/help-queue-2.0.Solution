d37a473d934cfddf054f465086369277b23a0cb8
Curtis Brooks <curtisbrooks678@gmail.com>
2022-02-08 09:54:17 -0800

    add questions before form button
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,21 +7,41 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      allowedToSubmit: 0
     };
   }
 
   handleClick = () => {
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+      allowedToSubmit: 0
+    }));
+  }
+
+  advanceText = () => {
+    this.setState(prevState => ({
+      allowedToSubmit: prevState.allowedToSubmit += 1
     }));
   }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
+    let addAdvanceButton = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
+      if (this.state.allowedToSubmit === 0) {
+        currentlyVisibleState = <p>"Have you gone through all the steps on the Learn How to Program debugging lesson?"</p>;
+      } else if (this.state.allowedToSubmit === 1) {
+        currentlyVisibleState = <p>"Have you asked another pair for help?"</p>;
+      } else if (this.state.allowedToSubmit === 2) {
+        currentlyVisibleState = <p>"Have you spent 15 minutes going through through the problem documenting every step?"</p>;
+      } else if (this.state.allowedToSubmit >= 3) {
+        currentlyVisibleState = <NewTicketForm />;
+      }
+      if (this.state.allowedToSubmit < 3) {
+        addAdvanceButton = <button onClick={this.advanceText} class="btn btn-warning">Yes I did!</button>;
+      }
       buttonText = "Return to ticket list";
     } else {
       currentlyVisibleState = <TicketList />;
@@ -30,6 +50,7 @@ class TicketControl extends React.Component {
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        {addAdvanceButton}
         <button onClick={this.handleClick} class="btn btn-info">{buttonText}</button>
       </React.Fragment>
     );
